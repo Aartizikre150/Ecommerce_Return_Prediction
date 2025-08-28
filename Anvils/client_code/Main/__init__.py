212d48b680ae86dfb91d6c2c0a35bbdd7f0e59b4
AartiZikre <zikreaarti1998@gmail.com>
2025-08-28 10:21:19 -0400

    Final changes of anvil

---
+++ Anvils/client_code/Main/__init__.py
@@ -0,0 +1,58 @@
+from ._anvil_designer import MainTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from anvil import open_form
+from ..About import About
+from ..Contact import Contact
+from ..FAQ import FAQ
+from ..Home import Home
+
+
+class Main(MainTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.content_panel.add_component(Home(), full_width_row=True)
+
+    
+  def contact_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(Contact(), full_width_row=True)
+
+  def about_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(About(), full_width_row=True)
+
+  def home_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(Home(), full_width_row=True)
+
+  def bottom_about_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.about_link_click()
+
+  def bottom_contact_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.contact_link_click()
+
+  def faq_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(FAQ(), full_width_row=True)
+
+  def lets_begin_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(Home(self.lets_begin_click), full_width_row=True)
+    #self.content_panel.get_components()
+    #self.content_panel.add_component(self.card_3, full_width_row=True)
+
+
+
+
